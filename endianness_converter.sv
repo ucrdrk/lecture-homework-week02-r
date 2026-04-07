7ba9b116720aba3f6017d495cf8ea85fe6e8f6b1
Allan Knight <67027104+ucrdrk@users.noreply.github.com>
2026-04-07 14:24:59 -0700

    Implement endianness converter module

--- endianness_converter.sv
+++ endianness_converter.sv
@@ -1,1 +1,19 @@
 // Put your code below
+// Put your code below
+module endianness_converter(
+    input clk, 
+    input wire [31:0] word, 
+    input wire endianness_in, 
+    input wire endianess_out, 
+    output reg [31:0] converted_word
+);
+
+always @(posedge clk) begin
+    if (endianness_in == endianess_out) begin
+        converted_word <= word;
+    end else begin
+        converted_word <= {word[7:0], word[15:8], word[23:16], word[31:24]};
+    end
+end
+
+endmodule
